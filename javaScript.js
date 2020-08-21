3b1978218e65e43c8f5f4f8f46d084b276426add
mgurn1990 <64208256+mgurn1990@users.noreply.github.com>
2020-08-21 16:12:22 +0100

    toasts!

--- javaScript.js
+++ javaScript.js
@@ -3,11 +3,16 @@ Let's think about what kind of functions I will need:
 1. onclick take to specific page
 2. refresh "there are 5 jobs" how often? 30seconds
  */
-$('.alert').alert()
 
 
-$('.toast').toast({
-    'autohide': false
-  });
 
-  $('.toast').toast('show');
+function getToast() {
+    // Get the toast DIV
+    var x = document.getElementById("toast");
+  
+    // this adds the "show" class to DIV
+    x.className = "show";
+  
+    // After 10 seconds, this removes the show class from DIV
+    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 10000);
+  }
